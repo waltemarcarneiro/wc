43e97715c5fabbf308f781470405d1055fa3bea3
Waltemar <78711339+waltemarcarneiro@users.noreply.github.com>
2022-10-22 05:35:20 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,14 @@
-self.addEventListener('fetch', function(event) {});
+// sw.js
+self.addEventListener('install', e => {
+ e.waitUntil(
+   // depois que o Service Worker estiver instalado,,
+   // abra um novo cache
+   caches.open('my-pwa-cache').then(cache => {
+     // adicione todas as URLs de recursos que queremos armazenar em cache
+     return cache.addAll([
+       '/',
+       'https://waltemarcarneiro.blogspot.com/index.html',
+     ]);
+   })
+ );
+});
