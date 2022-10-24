6f71fbd52070f52d2959159ccf466e5829c41421
Waltemar <78711339+waltemarcarneiro@users.noreply.github.com>
2022-10-24 09:02:08 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,52 @@
-// sw.js
-self.addEventListener('install', e => {
- e.waitUntil(
-   // depois que o Service Worker estiver instalado,,
-   // abra um novo cache
-   caches.open('my-pwa-cache').then(cache => {
-     // adicione todas as URLs de recursos que queremos armazenar em cache
-     return cache.addAll([
-       '/',
-       'https://waltemarcarneiro.blogspot.com/index.html',
-     ]);
-   })
- );
+const CACHE_NAME = 'offline';
+const OFFLINE_URL = 'https://waltemarcarneiro.blogspot.com/p/offline.html';
+
+self.addEventListener('install', function(event) {
+  console.log('[ServiceWorker] Install');
+  
+  event.waitUntil((async () => {
+    const cache = await caches.open(CACHE_NAME);
+    // Setting {cache: 'reload'} in the new request will ensure that the response
+    // isn't fulfilled from the HTTP cache; i.e., it will be from the network.
+    await cache.add(new Request(OFFLINE_URL, {cache: 'reload'}));
+  })());
+  
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  console.log('[ServiceWorker] Activate');
+  event.waitUntil((async () => {
+    // Enable navigation preload if it's supported.
+    // See https://developers.google.com/web/updates/2017/02/navigation-preload
+    if ('navigationPreload' in self.registration) {
+      await self.registration.navigationPreload.enable();
+    }
+  })());
+
+  // Tell the active service worker to take control of the page immediately.
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(event) {
+  // console.log('[Service Worker] Fetch', event.request.url);
+  if (event.request.mode === 'navigate') {
+    event.respondWith((async () => {
+      try {
+        const preloadResponse = await event.preloadResponse;
+        if (preloadResponse) {
+          return preloadResponse;
+        }
+
+        const networkResponse = await fetch(event.request);
+        return networkResponse;
+      } catch (error) {
+        console.log('[Service Worker] Fetch failed; returning offline page instead.', error);
+
+        const cache = await caches.open(CACHE_NAME);
+        const cachedResponse = await cache.match(OFFLINE_URL);
+        return cachedResponse;
+      }
+    })());
+  }
 });
